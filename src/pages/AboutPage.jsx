f5e4239c909e58e39ebfdfe33df3e97ac842bad9
NoName <86009604+itsnotismailov@users.noreply.github.com>
2022-12-26 19:24:32 +0600

    sa

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div style={{position:"absolute",left: '300px',fontWeight: "900",fontSize:"30px"}}>
+      <Link to='/'>
+        come back
+      </Link>
+    </div>
   )
 }
 
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react"
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const MainPage = () => {
-  const [state, setstate] = useState(0);
+  const [state, setstate] = useState(100);
+  const [input, setinput] = useState("hi im islam");
   return (
     <div>
-      <button>add</button>
+      <h1>{state}</h1>
+      <button onClick={() => setstate (state + 2)}>inc</button>
+      <button onClick={() => setstate(state - 10)}>dec</button>
+      <h2>{input}</h2>
+      <Link to='/about'>
+      воон
+      </Link>
+      
+      <input placeholder="search........ so cool" type="text" onChange={(e) => setinput(e.target.value)} />
     </div>
   )
 }
